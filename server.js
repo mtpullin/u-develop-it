1a8d4d178785616f6787c1db3a2e02256a7db056
mtpullin <mtpullin@gmail.com>
2021-07-06 13:00:21 -0400

    added first query

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+const mysql = require('mysql2')
 //express middlewear
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your MySQL username,
+        user: 'root',
+        //mysql password
+        password: '1darkness',
+        database: 'election'
+    },
+    console.log( 'Connected to the election database.')
+)
+db.query(`SELECT * FROM candidates`,(err, rows) => {
+    console.log(rows);
+})
 //default response for any other request
 app.use((req, res) => {
     res.status(404).end();
